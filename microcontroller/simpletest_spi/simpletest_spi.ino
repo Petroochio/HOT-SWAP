69b6276a18b36aa216ab66a0d1eb87b3122c1f9c
clementzheng <clement.zheng@gmail.com>
2018-10-12 11:25:14 -0600

    SPI test firmware

---
+++ microcontroller/simpletest_spi/simpletest_spi.ino
@@ -0,0 +1,41 @@
+/***************************************************
+Simple example of reading the MCP3008 analog input channels and printing
+them all out.
+
+Author: Carter Nelson
+License: Public Domain
+****************************************************/
+
+#include <Adafruit_MCP3008.h>
+
+Adafruit_MCP3008 adc1;
+//Adafruit_MCP3008 adc2;
+
+int count = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("MCP3008 simple test.");
+
+
+  adc1.begin(13, 11, 12, 10); //arguments: sck, mosi, miso, cs
+  //adc2.begin(13, 11, 12, 9);
+ 
+}
+
+void loop() {
+  for (int chan=0; chan<8; chan++) {
+    Serial.print(adc1.readADC(chan)); Serial.print("\t");
+  }
+  
+//  for (int chan=0; chan<8; chan++) {
+//    Serial.print(adc2.readADC(chan)); Serial.print("\t");
+//  }
+  
+  Serial.println();
+  count++;
+  
+  delay(10);
+}
